d8f75a06af63378cc8e45a25a71b8b833bdbfe31
syedmjavaid <syedmjavaid@gmail.com>
2022-08-09 11:34:31 +0100

    Install and Initialise firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXjrWB7NVMwrxe-q7yViuL3ZJ5wVfzEps",
+  authDomain: "lap4-3fe21.firebaseapp.com",
+  projectId: "lap4-3fe21",
+  storageBucket: "lap4-3fe21.appspot.com",
+  messagingSenderId: "527520539286",
+  appId: "1:527520539286:web:f0b567a7ab915ec85058b7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
