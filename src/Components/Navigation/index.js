b1877606d62829bbd59648c590826927992605c9
sigltech <101203107+sigltech@users.noreply.github.com>
2022-08-09 16:12:06 +0100

    added inbox sidebar

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import * as HiIcons from "react-icons/hi";
+import BurgerInbox from "../BurgerInbox";
+import InboxIcon from '@mui/icons-material/Inbox';
 import { SidebarData } from "./SidebarData";
 import { IconContext } from "react-icons";
 import "./burger.css";
 
 function Nav() {
   const [sidebar, setSidebar] = useState(false);
+  const [inboxSidebar, setInboxSidebar] = useState(false);
 
   const showSidebar = () => {
     setSidebar(!sidebar);
+  };
+
+  const showInboxSidebar = () => {
+    setInboxSidebar(!inboxSidebar);
   };
 
   return (
@@ -26,6 +34,11 @@ function Nav() {
               onClick={showSidebar}
             />
           </Link>
+          <div className="nav-inbox" onClick={showInboxSidebar}>
+            <div className="active-filter">
+            <InboxIcon className="nav-inbox-icon"/>
+            </div>
+          </div>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
@@ -44,6 +57,16 @@ function Nav() {
                 </li>
               );
             })}
+          </ul>
+        </nav>
+        <nav className={inboxSidebar ? "navbar-inbox active" : "navbar-inbox"}>
+          <ul className="nav-menu-items" onClick={showInboxSidebar}>
+            <li className="navbar-inbox-toggle">
+              <Link to="#" className="nav-menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            <BurgerInbox />
           </ul>
         </nav>
       </IconContext.Provider>
